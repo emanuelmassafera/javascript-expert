da659e4550eef7fd98d4852b6a5765102758d32a
Emanuel Massafera <emanuel301@live.com>
2023-03-17 13:19:02 -0300

    feat: add npm package class

---
+++ module-08-npm/demo-01-npm-package/app/index.js
@@ -0,0 +1,13 @@
+import FluentSQLBuilder from '@emanuelmassafera/fluent-sql'
+import database from './database/data.json' assert { type: "json" }
+
+const result = FluentSQLBuilder
+  .for(database)
+  .where({ registered: /^(2020|2019)/ })
+  .select(['category'])
+  .limit(3)
+  .countBy('category')
+  .build()
+
+
+console.log(result)
---
+++ module-08-npm/demo-01-npm-package/fluent-sql/index.js
@@ -0,0 +1,3 @@
+import FluentSQLBuilder from './src/fluent-sql.js'
+
+export default FluentSQLBuilder
